cf08a5c6507ef9279aed2d65c41fa06179e193d3
Patricio Avigliano <patricioavigliano@gmail.com>
2020-11-20 02:15:17 +0100

    read json

--- client/main.py
+++ client/main.py
@@ -1,16 +1,30 @@
 import os
+import time
 import pika
+import json
 
-
+REVIEWS_DATASET_FILEPATH = "data/yelp_academic_dataset_review.json"
+BUSINESS_DATASET_FILEPATH = "data/yelp_academic_dataset_business.json"
+CHUNK_SIZE = 4 * 1024
+MAX_REVIEWS = 6
 
 def main():
     amqp_url = os.environ['AMQP_URL']
     parameters = pika.URLParameters(amqp_url)
     connection = pika.BlockingConnection(parameters)
     channel = connection.channel()
-    channel.exchange_declare(exchange='reviews', exchange_type='direct')
-    for _ in range(5):
-        channel.basic_publish(exchange='reviews', routing_key="line", body="message")
+    channel.exchange_declare(exchange='data', exchange_type='direct')
+    with open(REVIEWS_DATASET_FILEPATH, 'r') as f:
+        review_count = 0
+        lines = f.readlines(CHUNK_SIZE)
+        while lines and review_count < MAX_REVIEWS:
+            message = ""
+            lines = f.readlines(CHUNK_SIZE)
+            reviews = [json.loads(line) for line in lines]
+            review_count += len(reviews)
+            message = json.dumps(reviews)
+            channel.basic_publish(exchange='data', routing_key="review", body=message)
+    time.sleep(5)
     connection.close()
 
 
--- server/main.py
+++ server/main.py
@@ -1,18 +1,24 @@
 import os
 import pika
+import json
 
 def callback(ch, method, properties, body):
-    print("[Routing Key: {} - Message: {}".format(method.routing_key, body))
+    ch.exchange_declare(exchange='reviews', exchange_type='direct')
+    reviews = json.loads(body)
+    for review in reviews:
+        funny = json.dumps(review)
+        print(funny, flush = True)
+        # channel.basic_publish(exchange='reviews', routing_key="funny", body=)
 
 def main():
     amqp_url = os.environ['AMQP_URL']
     parameters = pika.URLParameters(amqp_url)
     connection = pika.BlockingConnection(parameters)
     channel = connection.channel()
-    channel.exchange_declare(exchange='reviews', exchange_type='direct')
+    channel.exchange_declare(exchange='data', exchange_type='direct')
     result = channel.queue_declare(queue='', durable=True)
     queue_name = result.method.queue
-    channel.queue_bind(exchange='reviews', queue=queue_name, routing_key='line')
+    channel.queue_bind(exchange='data', queue=queue_name, routing_key='review')
     channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
     try:
         channel.start_consuming()
