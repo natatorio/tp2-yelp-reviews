a0a1008eb80cbd8a3b79aec3b71f5974961b4a7a
Patricio Avigliano <patricioavigliano@gmail.com>
2021-02-23 11:04:12 +0100

    quick query

--- users/main.py
+++ users/main.py
@@ -4,9 +4,9 @@ from consumers import CounterBy
 def main():
     counter = CounterBy(keyId="user_id", exchange="reviews", routing_key="users")
     user_count = counter.count()
-    user_count_5 = dict([u for u in user_count.items() if u[1] >= 5])
-    user_count_50 = dict([u for u in user_count_5.items() if u[1] >= 50])
-    user_count_150 = dict([u for u in user_count_50.items() if u[1] >= 1500])
+    user_count_5 = dict([u for u in user_count.items() if u[1] >= 3])
+    user_count_50 = dict([u for u in user_count_5.items() if u[1] >= 15])
+    user_count_150 = dict([u for u in user_count_50.items() if u[1] >= 100])
     counter.forward("reviews", "stars5", user_count_50)
     counter.forward("reviews", "comment", user_count_5)
 
