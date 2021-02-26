79dafe22db7e11ac4d383c19891fd92285b538bf
Patricio Avigliano <patricioavigliano@gmail.com>
2021-02-26 18:10:01 +0100

    leader election

--- health_server.py
+++ health_server.py
@@ -1,22 +1,52 @@
 import threading
-from flask import Flask
+from flask import Flask, make_response, jsonify
 import os
+import logging
 
 class HealthServer:
 
     def __init__(self):
-        self.t = threading.Thread(target=self.run)
+        self.app = Flask(__name__)
+        log = logging.getLogger('werkzeug')
+        log.setLevel(logging.ERROR)
+        self.__run_thread()
         self.t.setDaemon(True)
         self.t.start()
 
-    def run(self):
-        app = Flask(__name__)
+    def __run_thread(self):
+        self.t = threading.Thread(target=self.run_server)
 
-        @app.route('/health', methods=['GET'])
+    def run_server(self):
+        self.__route_health_endpoint()
+        self.app.run(debug=True, use_reloader=False, host='0.0.0.0', port=os.environ['HEALTHCHECK_PORT'])
+
+    def __route_health_endpoint(self):
+        @self.app.route('/health', methods=['GET'])
         def healthcheck():
             return ('', 204)
 
-        app.run(debug=True, use_reloader=False, host='0.0.0.0', port=os.environ['HEALTHCHECK_PORT'])
-
     def stop(self):
         exit(0)
+
+
+class LeaderServer(HealthServer):
+    def __init__(self, iAmLeader):
+        self.app = Flask(__name__)
+        log = logging.getLogger('werkzeug')
+        log.setLevel(logging.ERROR)
+        self.__run_thread(iAmLeader)
+        self.t.setDaemon(True)
+        self.t.start()
+
+    def __run_thread(self, iAmLeader):
+        self.t = threading.Thread(target=self.run_server, args=(iAmLeader,))
+
+    def run_server(self, iAmLeader):
+        self.iAmLeader = iAmLeader
+        self.__route_leader_endpoints()
+        super().run_server()
+
+    def __route_leader_endpoints(self):
+        @self.app.route('/id')
+        def get_id():
+            return make_response(jsonify( id=os.environ['HOSTNAME'], leader=self.iAmLeader[0] ), 200)
